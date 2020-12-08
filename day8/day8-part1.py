b8974ba817624ca738eefcff3c1f9ec258c9ab88
Ron van der Plas <r.vanderplas@homewizard.nl>
2020-12-08 08:24:39 +0100

    Part 1

--- day8/day8-part1.py
+++ day8/day8-part1.py
@@ -1,5 +1,5 @@
 def open_file_as_list():
-    f = open('temp.txt')
+    f = open('day8.txt')
     lines = f.readlines()
     file_list = list(lines)
     return file_list
@@ -12,19 +12,19 @@ def next_jump(f,index,accu):
     if "jmp" in split_cmd_value(f,index)[0]:
         return (index + split_cmd_value(f,index)[1]), accu
     elif "acc" in split_cmd_value(f,index)[0]:
-        return index+1, (accu + split_cmd_value(f,index))
+        return index+1, (accu + split_cmd_value(f,index)[1])
+    elif "nop" in split_cmd_value(f,index)[0]:
+        return index+1, accu
     else:
         return index, accu
-
 
 index = 0
 accu = 0
 unique_indexes = []
 f = open_file_as_list()
-if not index in unique_indexes:
+while not index in unique_indexes:
     unique_indexes.append(index)
     index, accu = next_jump(f,index, accu)
-    print(str(index) + " " + str(accu))
-else:
-    print(accu)
+    #print(str(index) + " " + str(accu))
+print(accu)
 
