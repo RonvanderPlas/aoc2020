b3924bf8ccc3aabe048d3b9fe0435fad49fffb49
Ron van der Plas <r.vanderplas@homewizard.nl>
2020-12-11 09:24:09 +0100

    Day 9 part 2

--- day9/day9-part2.py
+++ day9/day9-part2.py
@@ -1,35 +1,33 @@
 def open_file_as_list():
-    f = open('temp.txt')
+    f = open('day9.txt')
     lines = f.readlines()
     file_list = list(lines)
     return file_list
 
 def check_for_sum(f,min,max,length,new_value):
     new_list = f[min-1:max-1]
-    #print("Checking: " + str(int(f[new_value-1])) + " " + str(new_list))
     for value1 in new_list:
         for value2 in new_list:
             sum = int(value1)+int(value2)
             if sum == int(f[new_value-1]) and new_list.index(value1) != new_list.index(value2):
-                #print("1: " + str(int(value1)) + " 2: "  + str(int(value2)) +" Sum: " + str(sum) + " Checking: " + f[new_value-1])
                 return True
     return False
 
 def check_for_contigues(f,line_index,match_value):
     summed_value = 0
-    prev_value = 0
     summed_list = []
     while summed_value <= match_value:
-        if int(f[line_index]) > prev_value:
-            summed_value += int(f[line_index])
-            summed_list.append(str(int(f[line_index])))
-            prev_value = int(f[line_index])
-            line_index += 1
+        new_value = int(f[line_index])
+        summed_value += new_value
+        summed_list.append(new_value)
+        if len(summed_list) >= 2 and summed_value == match_value:
+            print("Found Match " + str(summed_list))
+            summed_list.sort()
+            print("First: " + str(summed_list[0]) + " Last: " + str(summed_list[-1]) + " Summed: " + str(summed_list[0]+summed_list[-1]))
 
-            if len(summed_list) > 2 and summed_value == match_value:
-                print("Found Match " + str(summed_list))
+        line_index += 1
 
-preamble_length = 5
+preamble_length = 25
 index = 0
 found_failed_value = 0
 f = open_file_as_list()
